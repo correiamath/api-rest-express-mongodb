99d4fd26072997ea89cd0a506f7cfa8151814d09
math <matheus.b.correia7@gmail.com>
2024-03-14 20:46:40 -0300

    adiciona conexão com mongoose

--- src/app.js
+++ src/app.js
@@ -1,55 +1,49 @@
 import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+  console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+  console.log("Conexao com o banco feita com sucesso");
+});
 
 const app = express();
 app.use(express.json());
-//Array com informações
-const livros = [
-  {
-    id: 1,
-    titulo: "O Senhor dos Anéis",
-  },
-  {
-    id: 2,
-    titulo: "O Hobbit",
-  },
-];
 
-//Rota raiz
 app.get("/", (req, res) => {
   res.status(200).send("Curso de Node.js");
 });
-//Rota para ler informações
-app.get("/livros", (req, res) => {
-  res.status(200).json(livros);
+
+app.get("/livros", async (req, res) => {
+  const listaLivros = await livro.find({});
+  res.status(200).json(listaLivros);
 });
-//Rota para criar informações
+
+app.get("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  res.status(200).json(livros[index]);
+});
+
 app.post("/livros", (req, res) => {
   livros.push(req.body);
   res.status(201).send("livro cadastrado com sucesso");
 });
-//Função para pesquisar com base em parâmetro
-function buscaLivro(id) {
-  return livros.findIndex((livro) => {
-    return livro.id === Number(id);
-  });
-}
-//Rota para ler informações pesquisadas
-app.get("/livros/:id", (req, res) => {
-  livros.push(req.body);
-  const index = buscaLivro(req.params.id);
-  res.status(200).json(livros[index]);
-});
-//Rota para atualizar informações
+
 app.put("/livros/:id", (req, res) => {
   const index = buscaLivro(req.params.id);
   livros[index].titulo = req.body.titulo;
   res.status(200).json(livros);
 });
-//Rota para deletar informações
+
 app.delete("/livros/:id", (req, res) => {
   const index = buscaLivro(req.params.id);
   livros.splice(index, 1);
-  res.status(200).send("Livro removido com sucesso");
+  res.status(200).send("livro removido com sucesso");
 });
 
 export default app;
